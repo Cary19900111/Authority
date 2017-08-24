ce8c67f7b1c7bd4c8c45f712c150296e3571252d
cary19900111 <1516945385@qq.com>
2017-08-24 17:28:53 +0800

    mysql+user配置

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,6 +24,8 @@ SECRET_KEY = '^y#g--n4n^zptdp_6t6qiz=#6$0p5t3sz))3@%f+p^q-l^2$vd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+#add cary
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -31,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # add cary
+    'face',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,13 +77,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'authority',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -116,5 +129,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+#add cary
+AUTH_USER_MODEL = 'face.User'
 
 STATIC_URL = '/static/'
