2128679aea6e4517857714c5ec94bc15a5a26415
cary19900111 <1516945385@qq.com>
2017-08-24 18:12:05 +0800

    version 0.1

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Authority
 Django about Authority
+$ python manage.py makemigrations app_name
+$ python manage.py migrate app_name
 
--- mysite/face/urls.py
+++ mysite/face/urls.py
@@ -1,3 +1,20 @@
 #！/usr/bin/env python
 #coding:utf-8
 __author__ = 'caryr'
+from django.conf.urls import url
+from . import views
+
+app_name = 'face'
+urlpatterns = [
+    # url(r'^$', views.IndexView.as_view(), name='index'),
+    # # ?P<question_id> defines the name that will be used to identify the matched pattern;
+    # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # # <li><a href="{% url 'detail' question.id %}">{{question.question_text}}</a></li> 通过name来控制
+    # url(r'^(?P<question_id>[0-9]+)/result/$', views.results, name='results'),
+    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    # url(r'^homepage',views.home,name='home'),
+    # url(r'^ci',views.ci,name='ci'),
+    # url(r'^jp', views.jp, name='jp'),
+    # url(r'^calc',views.test_all_dir_case,name='calc'),
+    # #url(r'^uploadfile',views.uploadfile,name='uploadfile'),
+]
